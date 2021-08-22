2128ff3034b50e4e2bee5d13f37aa79f76c8c9a1
Mitul Pradhan <43535786+Mitul514@users.noreply.github.com>
2021-08-22 19:06:53 +0530

    First Commit for Chaos Game
    -------- lol ------------

---
+++ Chaos_GameJam/Assets/Scripts/UI/UiManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    public void OnMenuPlayClicked()
+    {
+
+    }
+
+    public void OnExitMenuYesClicked()
+    {
+
+    }
+
+    public void OnExitMenuNoClicked()
+    {
+
+    }
+
+    public void PauseMenuResumeClicked()
+    {
+
+    }
+
+    public void PauseMenuExitClicked()
+    {
+
+    }
+}
